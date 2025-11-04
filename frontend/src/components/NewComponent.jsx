1c23640dd010fc9c7c58e214996222a7231f200e
DavidRengifo12 <davidsantiagorengifoguacheta@gmail.com>
2025-11-04 11:49:03 -0500

    realizando mas pruebas

---
+++ frontend/src/components/NewComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../global/global.css'
+
+export default function NewComponent() {
+  return (
+    <div className=''>
+      <h1>Hola componente nuevo</h1>
+    </div>
+  )
+}
